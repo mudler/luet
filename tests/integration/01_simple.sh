255f768cc09c4c049ab9475ee3152c1efdaea47e
Ettore Di Giacinto <mudler@gentoo.org>
2019-12-31 15:56:03 +0100

    Enhance simple integration test

--- tests/integration/01_simple.sh
+++ tests/integration/01_simple.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+
 export LUET_NOLOCK=true
 
 oneTimeSetUp() {
@@ -12,21 +12,25 @@ oneTimeTearDown() {
 
 testBuild() {
     mkdir $tmpdir/testbuild
-    luet build --tree "$ROOT_DIR/tests/fixtures/buildableseed" --destination $tmpdir/testbuild --compression gzip test/c-1.0
+    luet build --tree "$ROOT_DIR/tests/fixtures/buildableseed" --destination $tmpdir/testbuild --compression gzip test/c-1.0 > /dev/null
     buildst=$?
     assertEquals 'builds successfully' "$buildst" "0"
+    assertTrue 'create package dep B' "[ -e '$tmpdir/testbuild/b-test-1.0.package.tar.gz' ]"
+    assertTrue 'create package' "[ -e '$tmpdir/testbuild/c-test-1.0.package.tar.gz' ]"
 }
 
 testRepo() {
+    assertTrue 'no repository' "[ ! -e '$tmpdir/testbuild/repository.yaml' ]"
     luet create-repo --tree "$ROOT_DIR/tests/fixtures/buildableseed" \
     --output $tmpdir/testbuild \
     --packages $tmpdir/testbuild \
     --name "test" \
     --uri $tmpdir/testrootfs \
-    --type local
+    --type local > /dev/null
 
     createst=$?
     assertEquals 'create repo successfully' "$createst" "0"
+    assertTrue 'create repository' "[ -e '$tmpdir/testbuild/repository.yaml' ]"
 }
 
 testInstall() {
@@ -37,11 +41,18 @@ system-repositories:
                      type: "local"
                      uri: "$tmpdir/testbuild"
 EOF
-    luet install --config $tmpdir/luet.yaml --system-dbpath $tmpdir/testrootfs --system-target $tmpdir/testrootfs test/c-1.0
+    luet install --config $tmpdir/luet.yaml --system-dbpath $tmpdir/testrootfs --system-target $tmpdir/testrootfs test/c-1.0 > /dev/null
     installst=$?
     assertEquals 'install test successfully' "$installst" "0"
+    assertTrue 'package installed' "[ -e '$tmpdir/testrootfs/c' ]"
 }
 
+testUnInstall() {
+    luet uninstall --config $tmpdir/luet.yaml --system-dbpath $tmpdir/testrootfs --system-target $tmpdir/testrootfs test/c-1.0 > /dev/null
+    installst=$?
+    assertEquals 'uninstall test successfully' "$installst" "0"
+    assertTrue 'package uninstalled' "[ ! -e '$tmpdir/testrootfs/c' ]"
+}
 
 # Load shUnit2.
 . "$ROOT_DIR/tests/integration/shunit2"/shunit2
