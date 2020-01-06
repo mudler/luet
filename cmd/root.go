12c97c7a2aec4a804a96b6e87926bdc7c7667fdf
Ettore Di Giacinto <mudler@gentoo.org>
2020-01-06 19:11:44 +0100

    Prepare for 0.4 tag

--- cmd/root.go
+++ cmd/root.go
@@ -30,7 +30,7 @@ import (
 var cfgFile string
 var Verbose bool
 
-const LuetCLIVersion = "0.4-dev"
+const LuetCLIVersion = "0.4"
 
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
