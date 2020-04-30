b91288153a5d516b135b6f4a70a0d373d389289d
Ettore Di Giacinto <mudler@gentoo.org>
2020-04-30 21:48:46 +0200

    Make tree validation cmd concurrent

--- cmd/tree/validate.go
+++ cmd/tree/validate.go
@@ -17,11 +17,15 @@
 package cmd_tree
 
 import (
+	"errors"
 	"fmt"
 	"os"
+	"regexp"
 	"sort"
+	"strconv"
+	"sync"
 
-	//. "github.com/mudler/luet/pkg/config"
+	. "github.com/mudler/luet/pkg/config"
 	helpers "github.com/mudler/luet/pkg/helpers"
 	. "github.com/mudler/luet/pkg/logger"
 	pkg "github.com/mudler/luet/pkg/package"
@@ -30,6 +34,174 @@ import (
 
 	"github.com/spf13/cobra"
 )
+
+func validateWorker(i int,
+	wg *sync.WaitGroup,
+	c <-chan pkg.Package,
+	reciper tree.Builder,
+	withSolver bool,
+	regExcludes, regMatches []*regexp.Regexp,
+	excludes, matches []string,
+	errs chan error) {
+
+	defer wg.Done()
+
+	var depSolver solver.PackageSolver
+	brokenPkgs := 0
+	brokenDeps := 0
+	var errstr string
+
+	emptyInstallationDb := pkg.NewInMemoryDatabase(false)
+	if withSolver {
+		depSolver = solver.NewSolver(pkg.NewInMemoryDatabase(false),
+			reciper.GetDatabase(),
+			emptyInstallationDb)
+	}
+
+	for p := range c {
+		found, err := reciper.GetDatabase().FindPackages(
+			&pkg.DefaultPackage{
+				Name:     p.GetName(),
+				Category: p.GetCategory(),
+				Version:  ">=0",
+			},
+		)
+
+		if err != nil || len(found) < 1 {
+			if err != nil {
+				errstr = err.Error()
+			} else {
+				errstr = "No packages"
+			}
+			Error(fmt.Sprintf("%s/%s-%s: Broken. No versions could be found by database %s",
+				p.GetCategory(), p.GetName(), p.GetVersion(),
+				errstr,
+			))
+
+			errs <- errors.New(
+				fmt.Sprintf("%s/%s-%s: Broken. No versions could be found by database %s",
+					p.GetCategory(), p.GetName(), p.GetVersion(),
+					errstr,
+				))
+
+			brokenPkgs++
+		}
+
+		pkgstr := fmt.Sprintf("%s/%s-%s", p.GetCategory(), p.GetName(),
+			p.GetVersion())
+
+		validpkg := true
+
+		if len(matches) > 0 {
+			matched := false
+			for _, rgx := range regMatches {
+				if rgx.MatchString(pkgstr) {
+					matched = true
+					break
+				}
+			}
+
+			if !matched {
+				continue
+			}
+		}
+
+		if len(excludes) > 0 {
+			excluded := false
+			for _, rgx := range regExcludes {
+				if rgx.MatchString(pkgstr) {
+					excluded = true
+					break
+				}
+			}
+
+			if excluded {
+				continue
+			}
+		}
+		Info("Checking package "+fmt.Sprintf("%s/%s-%s", p.GetCategory(), p.GetName(), p.GetVersion()), "with", len(p.GetRequires()), "dependencies.")
+
+		all := p.GetRequires()
+		all = append(all, p.GetConflicts()...)
+		for _, r := range all {
+			var deps pkg.Packages
+			var err error
+			if r.IsSelector() {
+				deps, err = reciper.GetDatabase().FindPackages(
+					&pkg.DefaultPackage{
+						Name:     r.GetName(),
+						Category: r.GetCategory(),
+						Version:  r.GetVersion(),
+					},
+				)
+			} else {
+				deps = append(deps, r)
+			}
+
+			if err != nil || len(deps) < 1 {
+				if err != nil {
+					errstr = err.Error()
+				} else {
+					errstr = "No packages"
+				}
+				Error(fmt.Sprintf("%s/%s-%s: Broken Dep %s/%s-%s - %s",
+					p.GetCategory(), p.GetName(), p.GetVersion(),
+					r.GetCategory(), r.GetName(), r.GetVersion(),
+					errstr,
+				))
+
+				errs <- errors.New(
+					fmt.Sprintf("%s/%s-%s: Broken Dep %s/%s-%s - %s",
+						p.GetCategory(), p.GetName(), p.GetVersion(),
+						r.GetCategory(), r.GetName(), r.GetVersion(),
+						errstr))
+
+				brokenDeps++
+
+				validpkg = false
+
+			} else {
+
+				Debug("Find packages for dep",
+					fmt.Sprintf("%s/%s-%s", r.GetCategory(), r.GetName(), r.GetVersion()))
+
+				if withSolver {
+					Spinner(32)
+					solution, err := depSolver.Install(pkg.Packages{r})
+					ass := solution.SearchByName(r.GetPackageName())
+					if err == nil {
+						_, err = solution.Order(reciper.GetDatabase(), ass.Package.GetFingerPrint())
+					}
+					SpinnerStop()
+
+					if err != nil {
+
+						Error(fmt.Sprintf("%s/%s-%s: solver broken for dep %s/%s-%s - %s",
+							p.GetCategory(), p.GetName(), p.GetVersion(),
+							r.GetCategory(), r.GetName(), r.GetVersion(),
+							err.Error(),
+						))
+
+						errs <- errors.New(
+							fmt.Sprintf("%s/%s-%s: solver broken for Dep %s/%s-%s - %s",
+								p.GetCategory(), p.GetName(), p.GetVersion(),
+								r.GetCategory(), r.GetName(), r.GetVersion(),
+								err.Error()))
+
+						brokenDeps++
+						validpkg = false
+					}
+
+				}
+			}
+
+		}
+
+		if !validpkg {
+			brokenPkgs++
+		}
+	}
+}
 
 func NewTreeValidateCommand() *cobra.Command {
 	var excludes []string
@@ -46,13 +218,8 @@ func NewTreeValidateCommand() *cobra.Command {
 			}
 		},
 		Run: func(cmd *cobra.Command, args []string) {
-			var depSolver solver.PackageSolver
-			var errstr string
+			concurrency := LuetCfg.GetGeneral().Concurrency
 
-			errors := make([]string, 0)
-
-			brokenPkgs := 0
-			brokenDeps := 0
 			withSolver, _ := cmd.Flags().GetBool("with-solver")
 
 			reciper := tree.NewInstallerRecipe(pkg.NewInMemoryDatabase(false))
@@ -61,13 +228,6 @@ func NewTreeValidateCommand() *cobra.Command {
 				if err != nil {
 					Fatal("Error on load tree ", err)
 				}
-			}
-
-			emptyInstallationDb := pkg.NewInMemoryDatabase(false)
-			if withSolver {
-				depSolver = solver.NewSolver(pkg.NewInMemoryDatabase(false),
-					reciper.GetDatabase(),
-					emptyInstallationDb)
 			}
 
 			regExcludes, err := helpers.CreateRegexArray(excludes)
@@ -79,160 +239,44 @@ func NewTreeValidateCommand() *cobra.Command {
 				Fatal(err.Error())
 			}
 
-			for _, p := range reciper.GetDatabase().World() {
+			all := make(chan pkg.Package)
+			errs := make(chan error)
 
-				found, err := reciper.GetDatabase().FindPackages(
-					&pkg.DefaultPackage{
-						Name:     p.GetName(),
-						Category: p.GetCategory(),
-						Version:  ">=0",
-					},
-				)
+			var wg = new(sync.WaitGroup)
 
-				if err != nil || len(found) < 1 {
-					if err != nil {
-						errstr = err.Error()
-					} else {
-						errstr = "No packages"
-					}
-					Error(fmt.Sprintf("%s/%s-%s: Broken. No versions could be found by database %s",
-						p.GetCategory(), p.GetName(), p.GetVersion(),
-						errstr,
-					))
-
-					errors = append(errors,
-						fmt.Sprintf("%s/%s-%s: Broken. No versions could be found by database %s",
-							p.GetCategory(), p.GetName(), p.GetVersion(),
-							errstr,
-						))
-
-					brokenPkgs++
-				}
-
-				pkgstr := fmt.Sprintf("%s/%s-%s", p.GetCategory(), p.GetName(),
-					p.GetVersion())
-
-				validpkg := true
-
-				if len(matches) > 0 {
-					matched := false
-					for _, rgx := range regMatches {
-						if rgx.MatchString(pkgstr) {
-							matched = true
-							break
-						}
-					}
-
-					if !matched {
-						continue
-					}
-				}
-
-				if len(excludes) > 0 {
-					excluded := false
-					for _, rgx := range regExcludes {
-						if rgx.MatchString(pkgstr) {
-							excluded = true
-							break
-						}
-					}
-
-					if excluded {
-						continue
-					}
-				}
-				Info("Checking package "+fmt.Sprintf("%s/%s-%s", p.GetCategory(), p.GetName(), p.GetVersion()), "with", len(p.GetRequires()), "dependencies.")
-
-				all := p.GetRequires()
-				all = append(all, p.GetConflicts()...)
-				for _, r := range all {
-					var deps pkg.Packages
-					var err error
-					if r.IsSelector() {
-						deps, err = reciper.GetDatabase().FindPackages(
-							&pkg.DefaultPackage{
-								Name:     r.GetName(),
-								Category: r.GetCategory(),
-								Version:  r.GetVersion(),
-							},
-						)
-					} else {
-						deps = append(deps, r)
-					}
-
-					if err != nil || len(deps) < 1 {
-						if err != nil {
-							errstr = err.Error()
-						} else {
-							errstr = "No packages"
-						}
-						Error(fmt.Sprintf("%s/%s-%s: Broken Dep %s/%s-%s - %s",
-							p.GetCategory(), p.GetName(), p.GetVersion(),
-							r.GetCategory(), r.GetName(), r.GetVersion(),
-							errstr,
-						))
-
-						errors = append(errors,
-							fmt.Sprintf("%s/%s-%s: Broken Dep %s/%s-%s - %s",
-								p.GetCategory(), p.GetName(), p.GetVersion(),
-								r.GetCategory(), r.GetName(), r.GetVersion(),
-								errstr))
-
-						brokenDeps++
-
-						validpkg = false
-
-					} else {
-
-						Debug("Find packages for dep",
-							fmt.Sprintf("%s/%s-%s", r.GetCategory(), r.GetName(), r.GetVersion()))
-
-						if withSolver {
-							Spinner(32)
-							solution, err := depSolver.Install(pkg.Packages{r})
-							ass := solution.SearchByName(r.GetPackageName())
-							if err == nil {
-								_, err = solution.Order(reciper.GetDatabase(), ass.Package.GetFingerPrint())
-							}
-							SpinnerStop()
-
-							if err != nil {
-
-								Error(fmt.Sprintf("%s/%s-%s: solver broken for dep %s/%s-%s - %s",
-									p.GetCategory(), p.GetName(), p.GetVersion(),
-									r.GetCategory(), r.GetName(), r.GetVersion(),
-									err.Error(),
-								))
-
-								errors = append(errors,
-									fmt.Sprintf("%s/%s-%s: solver broken for Dep %s/%s-%s - %s",
-										p.GetCategory(), p.GetName(), p.GetVersion(),
-										r.GetCategory(), r.GetName(), r.GetVersion(),
-										err.Error()))
-
-								brokenDeps++
-								validpkg = false
-							}
-
-						}
-					}
-
-				}
-
-				if !validpkg {
-					brokenPkgs++
-				}
+			for i := 0; i < concurrency; i++ {
+				wg.Add(1)
+				go validateWorker(i, wg, all,
+					reciper, withSolver, regExcludes, regMatches, excludes, matches,
+					errs)
 			}
+			for _, p := range reciper.GetDatabase().World() {
+				all <- p
+			}
+			close(all)
 
-			sort.Strings(errors)
-			for _, e := range errors {
+			// Wait separately and once done close the channel
+			go func() {
+				wg.Wait()
+				close(errs)
+			}()
+
+			stringerrs := []string{}
+			for e := range errs {
+				stringerrs = append(stringerrs, e.Error())
+			}
+			sort.Strings(stringerrs)
+			for _, e := range stringerrs {
 				fmt.Println(e)
 			}
-			fmt.Println("Broken packages:", brokenPkgs, "(", brokenDeps, "deps ).")
 
-			if brokenPkgs > 0 {
-				os.Exit(1)
+			// fmt.Println("Broken packages:", brokenPkgs, "(", brokenDeps, "deps ).")
+			if len(stringerrs) != 0 {
+				Fatal("Errors: " + strconv.Itoa(len(stringerrs)))
+				//	if brokenPkgs > 0 {
+				//os.Exit(1)
 			} else {
+				Info("All good! :white_check_mark:")
 				os.Exit(0)
 			}
 		},
