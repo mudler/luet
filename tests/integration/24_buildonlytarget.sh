1331c3551a9b55a24a6c951a2a3427ba55f2b59a
Ettore Di Giacinto <mudler@sabayon.org>
2020-12-19 16:00:59 +0100

    Renaming clashing test func

--- tests/integration/24_buildonlytarget.sh
+++ tests/integration/24_buildonlytarget.sh
@@ -15,17 +15,15 @@ testBuild() {
     luet build --tree "$ROOT_DIR/tests/fixtures/simple_dep" --destination $tmpdir/testbuild1 test/c
     buildst=$?
     assertEquals 'builds successfully' "$buildst" "0"
-    assertTrue 'create package B 1.1' "[ -e '$tmpdir/testbuild1/b-test-1.1.package.tar' ]"
     assertTrue 'create package A 1.2' "[ -e '$tmpdir/testbuild1/a-test-1.2.package.tar' ]"
     assertTrue 'create package C 1.0' "[ -e '$tmpdir/testbuild1/c-test-1.0.package.tar' ]"
 }
 
-testBuild() {
+testBuildOnlyTarget() {
     mkdir $tmpdir/testbuild2
     luet build --tree "$ROOT_DIR/tests/fixtures/simple_dep" --destination $tmpdir/testbuild2 --only-target-package test/c
     buildst=$?
     assertEquals 'builds successfully' "$buildst" "0"
-    assertTrue 'create package B 1.1' "[ ! -e '$tmpdir/testbuild2/b-test-1.1.package.tar' ]"
     assertTrue 'create package A 1.2' "[ ! -e '$tmpdir/testbuild2/a-test-1.2.package.tar' ]"
     assertTrue 'create package C 1.0' "[ -e '$tmpdir/testbuild2/c-test-1.0.package.tar' ]"
 }
