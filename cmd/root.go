c536aaa53c34dcc7a42d8bc64adce7ede0c1cd27
Ettore Di Giacinto <mudler@sabayon.org>
2021-10-10 21:27:35 +0200

    Tag 0.18.1

--- cmd/root.go
+++ cmd/root.go
@@ -41,7 +41,7 @@ var Verbose bool
 var LockedCommands = []string{"install", "uninstall", "upgrade"}
 
 const (
-	LuetCLIVersion = "0.18.0"
+	LuetCLIVersion = "0.18.1"
 	LuetEnvPrefix  = "LUET"
 	license        = `
 	Luet Copyright (C) 2019-2021 Ettore Di Giacinto
